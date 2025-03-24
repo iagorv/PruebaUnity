9526a2a87597412bcca977fb30d07f88f48426fb
iagorv <Iagorive18@gmail.com>
2025-03-24 15:41:49 +0100

    Corregido sprite muerte

---
+++ Assets/Scripts/StateMachine/FadeRemoveBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FadeRemoveBehaviour : StateMachineBehaviour
+{
+        private float fadeTime = 0.5f;
+
+        private float timeElapsed = 0f;
+
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+ 
+}
